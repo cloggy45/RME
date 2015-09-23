60466fa12889c12234974611f945afc1de193a57
cloggy45 <cloggy45@googlemail.com>
2015-09-24 00:23:07 +0100

    Final Build

--- client/pages/account_settings/events/faction_settings.js
+++ client/pages/account_settings/events/faction_settings.js
@@ -1,14 +1,21 @@
 Template.userFactionSettings.events({
    "submit form" : function(event, template) {
        event.preventDefault();
+
        var factionName = event.target.selectFaction.value;
-       Meteor.users.update(Meteor.userId(), {$set: {'profile.faction':factionName}}, function(error) {
-           if(error) {
-               sAlert.error(error,{effect:'scale', timeout:5000});
-           } else {
-               sAlert.success('Faction updated!',{effect:'scale', timeout:2000});
-           }
-       });
-       template.find("form").reset();
+
+       if(factionName !== 'Select Faction') {
+           Meteor.users.update(Meteor.userId(), {$set: {'profile.faction':factionName}}, function(error) {
+               if(error) {
+                   sAlert.error(error,{effect:'scale', timeout:5000});
+               } else {
+                   sAlert.success('Faction updated!',{effect:'scale', timeout:2000});
+               }
+           });
+           template.find("form").reset();
+       } else {
+           sAlert.error('Select a faction',{effect:'scale', timeout:5000});
+       }
+
    }
 });
